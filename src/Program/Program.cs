c5d95ea2b909bd5eaa66d47cf0e6cae2e39d3c2f
Juan05Bernal <juaningenierobernal616@gmail.com>
2026-08-06 11:58:53 -0300

    implementacion de la operacion Division

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,5 +6,13 @@
         Console.WriteLine(Subtraction.Subtract(3, 4));
         Console.WriteLine(Multiplication.Multiply(5, 6));
         Console.WriteLine(Division.Divide(7, 8));
+      
     }
+}
+public class Division
+{
+   public static double Divide(int a, int b)
+   {
+       return (double)a / b;
+   }
 }
